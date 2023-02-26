7bf4e4a3e89081d984143a4e7e76bef77fc83b04
Vanessa O <vaneolipa@gmail.com>
2023-02-26 17:05:46 -0500

    Create variables

---
+++ src/modules/variables.js
@@ -0,0 +1,11 @@
+// form fields
+export const petInput = document.querySelector('#pet');
+export const ownerInput = document.querySelector('#owner');
+export const phoneInput = document.querySelector('#phone');
+export const dateInput = document.querySelector('#date');
+export const hourInput = document.querySelector('#hour');
+export const symptomsInput = document.querySelector('#symptoms');
+
+// UI
+export const form = document.querySelector('#new-appointment');
+export const containerAppointments = document.querySelector('#appointments');
